101ab81ac88173d4ea39157a204421cdf53468ee
unknown <vendettaktl@gmail.com>
2025-07-12 20:12:07 +0700

    auth functionality

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,68 @@
-import { router } from "expo-router";
-import { Button, Text, View } from "react-native";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { createUser } from "@/lib/appwrite";
+import { Link, router } from "expo-router";
+import { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignUp = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ name: "", email: "", password: "" });
+
+  const submit = async () => {
+    const { name, email, password } = form;
+
+    if (!name || !email || !password)
+      return Alert.alert(
+        "Kesalahan",
+        "Tolong lengkapi form yang masih kosong."
+      );
+
+    setIsSubmitting(true);
+
+    try {
+      await createUser({ email, password, name });
+
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>signUp</Text>
-      <Button title="Sign In" onPress={() => router.push("/sign-in")} />
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
+        placeholder="Masukkan nama anda"
+        value={form.name}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, name: text }))}
+        label="Nama"
+      />
+      <CustomInput
+        placeholder="Masukkan alamat email anda"
+        value={form.email}
+        onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+        label="Email"
+        keyboardType="email-address"
+      />
+      <CustomInput
+        placeholder="Masukkan password anda"
+        value={form.password}
+        onChangeText={(text) =>
+          setForm((prev) => ({ ...prev, password: text }))
+        }
+        label="Password"
+        secureTextEntry={true}
+      />
+      <CustomButton title="Daftar" isLoading={isSubmitting} onPress={submit} />
+
+      <View className="flex justify-center mb-3 flex-row gap-2">
+        <Text className="base-regular text-gray-100">Sudah memiliki akun?</Text>
+        <Link href="/sign-in" className="base-bold text-primary">
+          Masuk
+        </Link>
+      </View>
     </View>
   );
 };
